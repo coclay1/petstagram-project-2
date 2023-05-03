8b48fcce208f6c81992492ccb2978de05dcdacac
alboy77 <al.boucetta@yahoo.com>
2023-05-03 15:05:03 -0400

    90% done

--- controllers/API/user-routes.js
+++ controllers/API/user-routes.js
@@ -1,17 +1,17 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-router.get('/', async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      });
+// router.get('/', async (req, res) => {
+//   try {
+//     const userData = await User.findAll({
+//       });
 
-      res.status(200).json(userData);
+//       res.status(200).json(userData);
   
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 //CREATE NEW USER
 router.post('/', async (req, res) => {
