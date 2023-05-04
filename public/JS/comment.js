08b4e252d8ef1c3d7132a5ee7b9b8e4cfc6a025a
alboy77 <al.boucetta@yahoo.com>
2023-05-04 13:16:00 -0400

    trying to figure out comments

--- public/JS/comment.js
+++ public/JS/comment.js
@@ -1,24 +1,65 @@
+// const commentFormHandler = async function (event) {
+//     event.preventDefault();
+
+//     const body = document.querySelector('#comment').value;
+
+//     if (body) {
+//         await fetch('/api/comments', {
+//             method: 'POST',
+//             body: JSON.stringify({
+//                 body
+//             }),
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             }
+//         });
+//         // document.location.reload();
+//     }
+// };
+
+// document
+//     .querySelector('#commentSubmit')
+//     .addEventListener('submit', commentFormHandler);
+
+const commentsSection = document.querySelector('#comments');
+commentsSection.appendChild(commentElement);
+
 const commentFormHandler = async function (event) {
     event.preventDefault();
-
+  
     const body = document.querySelector('#comment').value;
-
+  
     if (body) {
-        await fetch('/api/comments', {
-            method: 'POST',
-            body: JSON.stringify({
-                body
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
+      try {
+        const response = await fetch('/api/comments', {
+          method: 'POST',
+          body: JSON.stringify({
+            body
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
         });
-        // document.location.reload();
+        const data = await response.json();
+  
+        // create a new comment element
+        const commentElement = document.createElement('div');
+        commentElement.classList.add('comment');
+        commentElement.innerHTML = `
+          <p>Comment by ${data.user}:</p>
+          <p>${data.body}</p>
+        `;
+  
+        // add the new comment element to the comments section
+        const commentsSection = document.querySelector('#comments');
+        commentsSection.appendChild(commentElement);
+  
+        // clear the comment input
+        document.querySelector('#comment').value = '';
+      } catch (err) {
+        console.error(err);
+      }
     }
-};
-
-document
-    .querySelector('#commentSubmit')
-    .addEventListener('submit', commentFormHandler);
-
-
+  };
+  
+  document.querySelector('#commentSubmit').addEventListener('submit', commentFormHandler);
