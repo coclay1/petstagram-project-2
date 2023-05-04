ff3f8f4a977fc0d381c2b04f652f3e16dcd1e5d3
alboy77 <al.boucetta@yahoo.com>
2023-05-04 15:04:40 -0400

    comments working

--- public/JS/comment.js
+++ public/JS/comment.js
@@ -1,40 +1,58 @@
 
 const commentFormHandler = async function (event) {
     event.preventDefault();
-  
+
     const body = document.querySelector('#comment').value;
-  
+
     if (body) {
-      const response = await fetch('/api/comments', {
-        method: 'POST',
-        body: JSON.stringify({
-          body
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      const data = await response.json();
-      console.log(data)
-  
-      // create a new comment element
-      const commentElement = document.createElement('div');
-      commentElement.classList.add('comment');
-      commentElement.innerHTML = `
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                body
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        console.log(data)
+
+        // create a new comment element
+        const commentElement = document.createElement('div');
+        commentElement.classList.add('comment');
+        commentElement.innerHTML = `
         <p>Comment by User Number: ${data.user_id}</p>
     
         <p>${data.body}</p>
       `;
-  
-      // add the new comment element to the comments section
-      const commentsSection = document.querySelector('#comments');
-      commentsSection.appendChild(commentElement);
-  
-      // clear the comment input
-      document.querySelector('#comment').value = '';
+
+        // add the new comment element to the comments section
+        const commentsSection = document.querySelector('#comments');
+        commentsSection.appendChild(commentElement);
+
+        const comments = JSON.parse(localStorage.getItem('comments')) || [];
+        comments.push(data);
+        localStorage.setItem('comments', JSON.stringify(comments));
+
+        // clear the comment input
+        document.querySelector('#comment').value = '';
     }
-  };
-  
-  document
+};
+
+document
     .querySelector('#commentSubmit')
     .addEventListener('submit', commentFormHandler);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const comments = JSON.parse(localStorage.getItem('comments')) || [];
+    comments.forEach(comment => {
+        const commentElement = document.createElement('div');
+        commentElement.classList.add('comment');
+        commentElement.innerHTML = `
+            <p>Comment by User Number: ${comment.user_id}</p>
+            <p>${comment.body}</p>
+          `;
+        const commentsSection = document.querySelector('#comments');
+        commentsSection.appendChild(commentElement);
+    });
+});
