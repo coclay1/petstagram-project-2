acffe1cc810be1c43ef3d0f309f82772f8e19854
alboy77 <al.boucetta@yahoo.com>
2023-05-05 03:38:10 -0400

    idk im going to sleep

--- public/JS/add-post.js
+++ public/JS/add-post.js
@@ -4,65 +4,96 @@ const bodyInput = document.querySelector('#body');
 let imageUrl = '';
 
 
+// addPostForm.addEventListener('submit', async (event) => {
+//   event.preventDefault();
+
+  
+//   const title = titleInput.value.trim();
+//   const body = bodyInput.value.trim();
+
+  
+//   if (!title || !body) {
+//     alert('Please fill out all fields.');
+//     return;
+//   }
+  
+//   if (!imageUrl) {
+//     alert('Please upload an image.');
+//     return;
+//   }
+
+  
+//   try {
+//     const response = await fetch('/api/posts', {
+//       method: 'POST',
+//       body: JSON.stringify({ title, body }),
+//       headers: { 'Content-Type': 'application/json' },
+//     });
+
+    
+//     if (response.ok) {
+//       window.location.replace('/');
+//     } else {
+//       alert('Failed to create post');
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     alert('Error creating post');
+//   }
+// });
+
+// document.getElementById("upload_widget").addEventListener("click", function(){
+//       myWidget.open();
+//     }, false);
+
+// var myWidget = cloudinary.createUploadWidget({
+//     cloudName: 'ddiyjjqhn', 
+//     uploadPreset: 'fbuysf1t'}, (error, result) => { 
+//       if (!error && result && result.event === "success") { 
+//         console.log('Done! Here is the image info: ', result.info); 
+//         imageUrl = result.info.url
+//       }
+//     });
+
+// const addPostFormHandler = async (event) => {
+//   event.preventDefault();
+
+//   const title = document.querySelector('#title').value.trim();
+//   const body = document.querySelector('#body').value.trim();
+//   const imageInput = document.querySelector('#image');
+  
+  
+
+//   if (title && body && imageUrl) {
+//     const response = await fetch('/api/posts', {
+//       method: 'POST',
+//       body: JSON.stringify({
+//         title,
+//         body,
+//         imageUrl
+//       }),
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     });
+
+//     if (response.ok) {
+//       document.location.replace('/');
+//     } else {
+//       alert('Failed to add post');
+//     }
+//   }
+// };
+
+// document.querySelector('#add-post-form').addEventListener('submit', addPostFormHandler);
+
+
 addPostForm.addEventListener('submit', async (event) => {
   event.preventDefault();
 
-  
   const title = titleInput.value.trim();
   const body = bodyInput.value.trim();
-
-  
-  if (!title || !body) {
-    alert('Please fill out all fields.');
-    return;
-  }
-  
-  if (!imageUrl) {
-    alert('Please upload an image.');
-    return;
-  }
-
-  
-  try {
-    const response = await fetch('/api/posts', {
-      method: 'POST',
-      body: JSON.stringify({ title, body }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    
-    if (response.ok) {
-      window.location.replace('/');
-    } else {
-      alert('Failed to create post');
-    }
-  } catch (error) {
-    console.error(error);
-    alert('Error creating post');
-  }
-});
-
-document.getElementById("upload_widget").addEventListener("click", function(){
-      myWidget.open();
-    }, false);
-
-var myWidget = cloudinary.createUploadWidget({
-    cloudName: 'ddiyjjqhn', 
-    uploadPreset: 'fbuysf1t'}, (error, result) => { 
-      if (!error && result && result.event === "success") { 
-        console.log('Done! Here is the image info: ', result.info); 
-        imageUrl = result.info.url
-      }
-    });
-
-const addPostFormHandler = async (event) => {
-  event.preventDefault();
-
-  const title = document.querySelector('#title').value.trim();
-  const body = document.querySelector('#body').value.trim();
   const imageInput = document.querySelector('#image');
-  
-  
 
   if (title && body && imageUrl) {
     const response = await fetch('/api/posts', {
@@ -83,6 +114,17 @@ const addPostFormHandler = async (event) => {
       alert('Failed to add post');
     }
   }
-};
+});
 
-document.querySelector('#add-post-form').addEventListener('submit', addPostFormHandler);
+document.getElementById("upload_widget").addEventListener("click", function(){
+      myWidget.open();
+    }, false);
+
+var myWidget = cloudinary.createUploadWidget({
+    cloudName: 'ddiyjjqhn', 
+    uploadPreset: 'fbuysf1t'}, (error, result) => { 
+      if (!error && result && result.event === "success") { 
+        console.log('Done! Here is the image info: ', result.info); 
+        imageUrl = result.info.url
+      }
+    });
