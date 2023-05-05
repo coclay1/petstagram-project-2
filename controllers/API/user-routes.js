12770f3560bdbe1e56deaae33288e880670bdbee
alboy77 <al.boucetta@yahoo.com>
2023-05-05 02:33:31 -0400

    YESS FINALLY COMMENTS PARSED HAHAHAHHAHAHA GOT IT

--- controllers/API/user-routes.js
+++ controllers/API/user-routes.js
@@ -1,18 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// router.get('/', async (req, res) => {
-//   try {
-//     const userData = await User.findAll({
-//       });
-
-//       res.status(200).json(userData);
-  
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
-
 //CREATE NEW USER
 router.post('/', async (req, res) => {
   try {
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-
 router.get('/', async (req, res) => {
   try {
     const postData = await Post.findAll({
